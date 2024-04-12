13b8c9d87b1ca5766fa5a6a14fae29220bda895c
JulienDLTT <149057454+JulienDLTT@users.noreply.github.com>
2024-04-12 16:36:46 +0200

    SOAP

---
+++ SOAP/client.js
@@ -0,0 +1,36 @@
+const soap = require("soap");
+
+soap.createClient("http://localhost:8000/products?wsdl", {}, function (err, client) {
+  if (err) {
+    console.error("Error creating SOAP client:", err);
+    return;
+  }
+  // Make a SOAP request
+  client.CreateProduct({ name: "My product", about : "blabla", price : "10" }, function (err, result) {
+    if (err) {
+      console.error(
+        "Error making SOAP request:",
+        err.response.status,
+        err.response.statusText,
+        err.body
+      );
+      return;
+    }
+    console.log("Result:", result);
+  });
+
+  client.GetProducts({}, function (err, result) {
+    if (err) {
+      console.error(
+        "Error making SOAP request:",
+        err.response.status,
+        err.response.statusText,
+        err.body
+      );
+      return;
+    }
+    console.log("Result:", result);
+  });
+
+
+});
